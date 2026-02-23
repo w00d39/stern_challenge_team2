4e04c3132ec8e748b304fec2b0c75a213b5bc4fb
Sarah Wood <wood39@iu.edu>
2026-02-22 23:10:09 -0500

    env setup for firebase credentials

--- backend/main.py
+++ backend/main.py
@@ -3,11 +3,16 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import firebase_admin
 from firebase_admin import credentials, firestore
+from dotenv import load_dotenv
+
+load_dotenv()
 
 #init firebase
-cred = credentials.Certificate("service-account.json")
+service_account_path = os.getenv("SERVICE_ACCOUNT_PATH")
+cred = credentials.Certificate(service_account_path)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
+
 
 
 app = FastAPI()
